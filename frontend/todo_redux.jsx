8585114e19845f69c45df197d8a89a2e79c2adb2
leol <ducleoliao@gmail.com>
2017-10-06 22:50:35 -0700

    Fixed selector

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { allTodos } from './reducers/selector';
+
+window.allTodos = allTodos;
 
 document.addEventListener('DOMContentLoaded', () => {
   // const preloadedState = localStorgage.state ? JSON.parse(localStorgage.state) : {};
