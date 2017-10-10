cdd6efd221ad5ead6b00c3fc7d374e17d67af899
leol <ducleoliao@gmail.com>
2017-10-09 23:51:55 -0700

    Updated todo_list.jsx

--- frontend/components/todo_list/todo_list.jsx
+++ frontend/components/todo_list/todo_list.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class TodoList extends React.components {
-  constructor(props){
-    super(props);
-  }
-
+  render(){
+    const {todos} = this.props;
+    const ListAllTodos = todos.map((todo, id) => (
+      <li key={todo.id}>
+        {todo.title}
+      </li>
+    ))
+    return (
+      <div>
+        <ul>
+          {ListAllTodos}
+        </ul>
+      </div>
+    )//end return
+  }//end render
 } //end TodoList
 
 export default TodoList;
